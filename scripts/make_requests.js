c0a853711abed766147715c6e11e5c9f601361f3
twister-dev <twister-cash@protonmail.com>
2022-07-06 03:06:55 -0700

    x

---
+++ scripts/make_requests.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const { BigNumber } = require('@ethersproject/bignumber');
+const { arrayify } = require('@ethersproject/bytes');
+const { pack } = require('@ethersproject/solidity');
+const {
+    calculateNextRoot,
+    poseidon: hasher,
+    utils: { unsafeRandomLeaves }
+} = require('vmtree-sdk');
+
+(async function() {
+    const startIndex = BigNumber.from(0);
+    const { filledSubtrees } = calculateNextRoot({ hasher });
+    const leaves = unsafeRandomLeaves(16);
+
+    try {
+        var requests = [];
+        for (let i = 0; i < 10; i++) {
+            requests.push(axios.post(
+                'http://localhost:8080/poseidonMassUpdate',
+                {
+                    data: {
+                        rawData: Buffer.from(arrayify(pack(
+                            (new Array(37)).fill('uint256'),
+                            [ startIndex, ...filledSubtrees, ...leaves ]
+                        ))).toString('base64')
+                    }
+                }
+            ));
+        }
+        const results = await Promise.all(requests);
+        console.log(result);
+    } catch(err) {
+        console.log(err);
+    }
+})();
