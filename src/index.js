e60eb737a941797161ed4a74c6cacd73e2afbe42
twister-dev <twister-cash@protonmail.com>
2022-07-06 22:57:46 -0700

    change default num workers

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const cluster = require('cluster');
 const app = require('./app');
 require('dotenv').config();
 
-const NUM_FORKS = process.env.NUM_FORKS || 1;
+const NUM_FORKS = process.env.NUM_FORKS || 0;
 const PORT = process.env.VMT_ADAPTER_PORT || 8080;
 
-if (cluster.isMaster) {
+if (NUM_FORKS === 0) {
+    app.listen(PORT, () => {
+        console.log(`Now listening on http://localhost:${PORT}`);
+    });
+} else if (cluster.isMaster) {
     console.log(`Master ${process.pid} is running.`);
 
     for (let i = 0; i < NUM_FORKS; i++) {
